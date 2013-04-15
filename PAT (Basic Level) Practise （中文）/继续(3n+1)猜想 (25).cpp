dcb7de9f38f17f2d0db4b3aa676380ec3f83ae79
tiny656 <tiny656@hotmail.com>
2013-04-15 19:19:59 +0800

    Update 继续(3n+1)猜想 (25).cpp

---
+++ PAT (Basic Level) Practise （中文）/继续(3n+1)猜想 (25).cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int f[100000];
+vector<int> ee;
+vector<int> ans;
+int main()
+{
+  int n,i,e;
+	memset(f, 0, sizeof(f));
+	cin >> n;
+	for(i = 1; i <= n; i++)
+	{
+		cin >> e;
+		ee.push_back(e);
+	}
+	for(i = 0; i < ee.size(); i++)
+	{
+		int  tmp = ee[i];
+		while(tmp != 1)
+		{
+			if(tmp & 1)	
+			{
+				tmp = (3*tmp+1) >> 1;
+			}
+			else
+			{
+				tmp >>= 1;
+			}
+			f[tmp] = 1;
+		}
+	}
+	for(i = 0; i < ee.size(); i++)
+	{
+		if(!f[ee[i]])	ans.push_back(ee[i]);
+	}
+	sort(ans.begin(), ans.end(), greater<int>());
+	cout << ans[0];
+	for(i = 1; i < ans.size(); i++)
+	{
+		cout << " " << ans[i];
+	}	
+	cout << endl;
+  return 0;
+}
