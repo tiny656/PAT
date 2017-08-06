c2ffbc4e32214468b27f3824203ba6072291d947
tiny656 <tiny656@hotmail.com>
2017-08-06 08:33:38 +0800

    update 1119

--- PAT (Advanced Level) Practise/1119_Pre- and Post-order Traversals (30).py
+++ PAT (Advanced Level) Practise/1119_Pre- and Post-order Traversals (30).py
@@ -9,21 +9,20 @@ def build(preLeft, preRight, postLeft, postRight):
     postRight -= 1
     size = preRight - preLeft + 1
     pos, find_count = -1, 0
+
     for i in xrange(size+1):
         preLeftList = preOrder[preLeft : preLeft+i]
         preRightList = preOrder[preLeft+i : preRight+1]
         postLeftList = postOrder[postLeft : postLeft+i]
         postRightList = postOrder[postLeft+i : postRight+1]
-        # print 'debug left', preLeftList, postLeftList
-        # print 'debug right', preRightList, postRightList
+
         if ((len(preLeftList) == 0 and len(postLeftList) == 0) \
                 or (sorted(preLeftList) == sorted(postLeftList) and preLeftList[0] == postLeftList[-1])) \
-        and ((len(preRightList) == 0 and len(postRightList) == 0) \
+            and ((len(preRightList) == 0 and len(postRightList) == 0) \
                 or (sorted(preRightList) == sorted(postRightList) and preRightList[0] == postRightList[-1])):
             pos = i
             find_count += 1
             max_find_count = max(max_find_count, find_count)
-            # print 'debug', pos, find_count
     if pos > 0: 
         build(preLeft, preLeft+pos-1, postLeft, postLeft+pos-1)
     ans.append(root)
