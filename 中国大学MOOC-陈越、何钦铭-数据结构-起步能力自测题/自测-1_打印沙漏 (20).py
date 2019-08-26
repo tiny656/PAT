f798f0e05705cf52f21f5dc4f32f7e7da61eb8a3
tiny656 <tiny656@hotmail.com>
2019-08-26 20:37:42 +0800

    update code

--- 中国大学MOOC-陈越、何钦铭-数据结构-起步能力自测题/自测-1_打印沙漏 (20).py
+++ 中国大学MOOC-陈越、何钦铭-数据结构-起步能力自测题/自测-1_打印沙漏 (20).py
@@ -7,12 +7,12 @@ n = int(n)
 lvl = int(math.sqrt((n+1)/2))
 total = 2*lvl**2-1 # fomula: Sn = 2*n^2 -1
 
-def BuildSandGlass(lvl, totLvl, ch):
+def BuildSandGlass(lvl, whitespace, ch):
     if (lvl == 1):
-        return ' ' * (totLvl-1) + ch
+        return ' ' * whitespace + ch
     else:
-        s = ' ' * (totLvl-lvl) + ch*(2*lvl-1)
-        return '%s\n%s\n%s' % (s, BuildSandGlass(lvl-1, totLvl, ch), s)
+        s = ' ' * whitespace + ch*(2*lvl-1)
+        return '%s\n%s\n%s' % (s, BuildSandGlass(lvl-1, whitespace+1, ch), s)
 
-print BuildSandGlass(lvl, lvl, ch)
+print BuildSandGlass(lvl, 0, ch)
 print n - total
