25a2b24b20698168ada4ac9318304116bbde96ec
tiny656 <tiny656@hotmail.com>
2019-09-13 08:56:43 +0800

    add top 1026

---
+++ PAT (Top Level) Practise/1026_String of Colorful Beads (35).cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+#include <tuple>
+#include <utility>
+using namespace std;
+
+/*
+最长不重复公共子串
+双指针 lo, hi 维护一个不重复的最长子序列，用unordered_set进行重复元素检查
+*/
+
+inline int max(int x, int y) {
+    return x > y ? x : y;
+}
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    vector<int> color, value;
+    color.resize(n); 
+    value.resize(n);
+
+    for (int i = 0; i < n; i++) cin >> value[i];
+    for (int i = 0; i < n; i++) cin >> color[i];
+
+    unordered_set<int> colorSet;
+    int lo = 0, hi = 0;
+    int curVal = 0, maxLen = 0;
+    tuple<int, int, int, int> ans(0, 0, 0, 0);
+    while (lo < n && hi < n) {
+        if (colorSet.find(color[hi]) == end(colorSet)) {
+            colorSet.insert(color[hi]);
+            curVal += value[color[hi]-1];
+            maxLen = max(maxLen, hi-lo+1);
+            if (maxLen > get<0>(ans)) {
+                get<0>(ans) = maxLen;
+                get<1>(ans) = curVal;
+                get<2>(ans) = lo;
+                get<3>(ans) = hi;
+            } else if (maxLen == get<0>(ans) && curVal > get<1>(ans)) {
+                get<1>(ans) = curVal;
+                get<2>(ans) = lo;
+                get<3>(ans) = hi;
+            }
+            hi += 1;
+        }
+        else {
+            colorSet.erase(color[lo]);
+            curVal -= value[color[lo]-1];
+            lo += 1;
+        }
+    }
+    cout << get<1>(ans) << " " << get<2>(ans) << " " << get<3>(ans) << endl;
+    return 0;
+}
--- build.bat
+++ build.bat
@@ -4,5 +4,5 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliar
 popd
 echo %cd%
 set compilerflags=/Od /EHsc /Zi
-set linkerflags=/OUT:%~n1.exe
+set linkerflags=/OUT:"%~n1.exe"
 cl.exe %compilerflags% %1 /link %linkerflags%
