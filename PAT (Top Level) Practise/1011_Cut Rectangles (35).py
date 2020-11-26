0797371d2248ca3fa7705088c0c119feac1699ab
tiny656 <tiny656@hotmail.com>
2020-11-26 21:25:13 +0800

    refactor

--- PAT (Top Level) Practise/1011_Cut Rectangles (35).py
+++ PAT (Top Level) Practise/1011_Cut Rectangles (35).py
@@ -7,6 +7,15 @@ def GetCross(p1, p2):
 def GetVector(p1, p2):
     return Point(p2.X-p1.X, p2.Y-p1.Y)
 
+def CheckCongruentTriangleSAS(t1, t2):
+    edgeLengthList, o_edgeLengthList = [], []
+    for i in range(3):
+        p1, p2 = t1[i], t1[(i+1) % 3]
+        edgeLengthList.append(GetDistanceSquare(p1, p2))
+        p1, p2 = t2[i], t2[(i+1) % 3]
+        o_edgeLengthList.append(GetDistanceSquare(p1, p2))
+    return sorted(edgeLengthList) == sorted(o_edgeLengthList)
+
 class Point(object):
     def __init__(self, x, y):
         self.X = x
@@ -43,13 +52,7 @@ class Polygon:
             hypotenuse = self.GetHypotenuse()
             o_hypotenuse = polygon.GetHypotenuse()
             if len(hypotenuse) == 1 and len(o_hypotenuse) == 1:
-                edgeLengthList, o_edgeLengthList = [], []
-                for i in range(3):
-                    p1, p2 = self.points[i], self.points[(i+1) % 3]
-                    edgeLengthList.append(GetDistanceSquare(p1, p2))
-                    p1, p2 = polygon.points[i], polygon.points[(i+1) % 3]
-                    o_edgeLengthList.append(GetDistanceSquare(p1, p2))
-                isRectangle = sorted(edgeLengthList) == sorted(o_edgeLengthList)
+                isRectangle = CheckCongruentTriangleSAS(self.points, polygon.points)
         
         # 3-4, 3-5
         # 各自只有一条斜边且相等，构造4,5边形的虚点，虚点和斜边之外的点要在斜边的两侧（确定虚点唯一位置），4,5边形斜边加虚点的三角形判全等
@@ -68,15 +71,9 @@ class Polygon:
                 di = GetCross(GetVector(refPoint, o_hypotenuse[0]), GetVector(refPoint, o_hypotenuse[1]))
                 di1 = GetCross(GetVector(candidateVirtualPoints[0], o_hypotenuse[0]), GetVector(candidateVirtualPoints[0], o_hypotenuse[1]))
                 vp = candidateVirtualPoints[0] if di*di1 < 0 else candidateVirtualPoints[1]
-
                 virtualTriangle = (vp, o_hypotenuse[0], o_hypotenuse[1])
-                edgeLengthList, o_edgeLengthList = [], []
-                for i in range(3):
-                    p1, p2 = virtualTriangle[i], virtualTriangle[(i+1) % 3]
-                    edgeLengthList.append(GetDistanceSquare(p1, p2))
-                    p1, p2 = tri.points[i], tri.points[(i+1) % 3]
-                    o_edgeLengthList.append(GetDistanceSquare(p1, p2))
-                isRectangle = sorted(edgeLengthList) == sorted(o_edgeLengthList)
+
+                isRectangle = CheckCongruentTriangleSAS(virtualTriangle, tri.points)
 
         # 4-4
         # 各自只有一条斜边且相等并且斜边相对的边相等 或者 没有斜边只要任意有一边相等
@@ -93,7 +90,6 @@ class Polygon:
                 o_remainPoints.remove(o_hypotenuse[1])
                 isOtherEdgeEq = GetDistanceSquare(remainPoints[0], remainPoints[1]) == GetDistanceSquare(o_remainPoints[0], o_remainPoints[1])
                 isRectangle = isHypotenuseEq and isOtherEdgeEq
-
             elif len(hypotenuse) == 0 and len(o_hypotenuse) == 0:
                 for i in range(self.count):
                     p1, p2 = self.points[i], self.points[(i+1) % self.count]
