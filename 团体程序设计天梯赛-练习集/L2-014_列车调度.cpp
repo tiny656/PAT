5377873c1bf4b775d7844a507ef1954604de3ec5
tiny656 <tiny656@hotmail.com>
2023-01-11 12:13:37 +0800

    update

--- 团体程序设计天梯赛-练习集/L2-014_列车调度.cpp
+++ 团体程序设计天梯赛-练习集/L2-014_列车调度.cpp
@@ -23,3 +23,38 @@ int main() {
     cout << s.size() << endl;
     return 0;
 }
+
+/* 
+//another implementation version
+#include <iostream>
+#include <vector>
+using namespace std;
+// Principle: maintain the set of the smallest end of the trains
+// 1. if coming train's number is larger than all exist numbers in ordered set, then add it to the set
+// 2. else, remove next larger train's number(use lower_bound/upper_bound to find it) in set, and add the new train to the set
+
+int UpperBound(const vector<int> &arr, int target) {
+    int low = 0, high = arr.size() - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] > target) high = mid - 1;
+        else low = mid + 1;
+    }
+    return low;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    int n, t;
+    vector<int> s;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> t;
+        int pos = UpperBound(s, t);
+        if (pos == s.size()) s.push_back(t); 
+        else s[pos] = t;
+    }
+    cout << s.size() << endl;
+    return 0;
+}
+*/
