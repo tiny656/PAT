8ac8624c07a5f66df9acece1a5e970caabeeecff
tiny656 <tiny656@hotmail.com>
2023-01-24 20:16:25 +0800

    update L2-022

--- 团体程序设计天梯赛-练习集/L2-022_重排链表.cpp
+++ 团体程序设计天梯赛-练习集/L2-022_重排链表.cpp
@@ -18,8 +18,8 @@ void PrintLinkList(int addr) {
 	}
 }
 
-// addr2 means iterator from left to right
 // addr1 means iterator from right to left
+// addr2 means iterator from left to right
 void DFS(int addr1, int &addr2, int &newHead, bool &isOk) {
 
 	if (addr1 == -1) return;
@@ -29,10 +29,10 @@ void DFS(int addr1, int &addr2, int &newHead, bool &isOk) {
 	// use recursive way to visit link list in reverse order
 	DFS(n1.next, addr2, newHead, isOk);
 
-	// if  already hit exit point
+	// if already hit exit point
 	if (isOk) return;
 
-	// case for only one node or node Ln
+	// case for only one node or node is Ln
 	if (n1.next == -1) {
 		if (addr1 != addr2) n1.next = addr2;
 		newHead = addr1;
@@ -53,19 +53,16 @@ void DFS(int addr1, int &addr2, int &newHead, bool &isOk) {
 		return;
 	}
 
-
-	// swap nodes, like L1->Ln-1->L2, then move L1 to next L2
+	// swap nodes, like L1->Ln-1->L2, then move addr2 to next
 	Node &n2 = nodes[addr2];
 	n1.next = n2.next;
 	n2.next = addr1;
 	addr2 = n1.next;
 }
 
-
-
 int main() {
-	int startNode, n, addr, data, next;
-	scanf("%d %d", &startNode, &n);
+	int head, n, addr, data, next;
+	scanf("%d %d", &head, &n);
 	for (int i = 0; i < n; i++) {
 		cin >> addr >> data >> next;
 		nodes[addr] = Node{data, next};
@@ -73,7 +70,7 @@ int main() {
 
 	int newHead = -1;
 	bool isOk = false;
-	DFS(startNode, startNode, newHead, isOk);
+	DFS(head, head, newHead, isOk);
 	PrintLinkList(newHead);
 
 	return 0;
